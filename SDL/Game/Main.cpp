47f5c79ae6eac6796cfff8649f60ca9dd97b893c
Unknown <goberna_ana@hotmail.com>
2018-04-25 11:17:20 -0300

    V0.1

--- SDL/Game/Main.cpp
+++ SDL/Game/Main.cpp
@@ -17,48 +17,53 @@ int main(int argc, char * argv[])
 	//}
 
 	SDL_Window *window;                    // Declare a pointer
-	SDL_Event event;
-	int quit = 0;
-	SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+	
+	SDL_Init(SDL_INIT_EVERYTHING);              // Initialize SDL2
 
 										   // Create an application window with the following settings:
 	window = SDL_CreateWindow(
-		"An SDL2 window",                  // window title
+		"Window",                  // window title
 		SDL_WINDOWPOS_UNDEFINED,           // initial x position
 		SDL_WINDOWPOS_UNDEFINED,           // initial y position
 		640,                               // width, in pixels
 		480,                               // height, in pixels
-		SDL_WINDOW_OPENGL                  // flags - see below
+		SDL_WINDOW_RESIZABLE                  // flags - see below
 	);
 
 	
 	// Check that the window was successfully created
 	if (window == NULL) {
 		// In the case that the window could not be made...
-		printf("Could not create window: %s\n", SDL_GetError());
+		cout << "Huston, tenemos un problema " << endl << SDL_GetError() << endl;
 		return 1;
 	}
 
-	// The window is open: could enter program loop here (see SDL_PollEvent())
+	//SDL_SetWindowBordered(window, SDL_FALSE);
+	SDL_SetWindowTitle(window, "Chimi");
+	int x, y, w, h;
+	SDL_Event event;
+	bool running = true;
 
-		//SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-
-						  // Close and destroy the window
-		//SDL_DestroyWindow(window);
-	
-		// Clean up
-		//SDL_Quit();
-	while (!quit) {
-		//Here the test
+	while (running) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT)
-				quit = 1;
-			else if (event.type == SDL_KEYDOWN)
-				printf("Hello\n");
+			if (event.type == SDL_QUIT) {
+				running = false;
+				break;
+			}
+		
 		}
+		SDL_GetWindowPosition(window, &x, &y);
+		cout << y << " , " << y << endl;
+	
 	}
+
+
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 
 	cin.get();
 
-	return 0;
+	
 }
